77694bde57663e47388d7996e7115cd135b9ecd7
Harish <harish.s2024cse@sece.ac.in>
2026-02-21 10:07:47 +0530

    Hardcode Render backend URL

--- src/config/api.js
+++ src/config/api.js
@@ -1,9 +1,5 @@
-const isDevelopment = window.location.hostname === 'localhost';
-const API_URL = isDevelopment 
-    ? 'http://localhost:3000'
-    : 'https://hostel-management-backend-bukg.onrender.com';
+const API_URL = 'https://hostel-management-backend-bukg.onrender.com';
 
 console.log('🔧 API_URL:', API_URL);
-console.log('🔧 Hostname:', window.location.hostname);
 
 export default API_URL;
